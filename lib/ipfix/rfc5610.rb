cdca802e6b69dfe564960027d7704935c05c52f3
Brian Trammell <brian@thaleia.trammell.ch>
2012-07-18 15:30:10 +0200

    fix syntax errors in rfc5610 support

--- lib/ipfix/rfc5610.rb
+++ lib/ipfix/rfc5610.rb
@@ -7,40 +7,41 @@
 #
 
 require_relative 'message'
-
-class IETypeOptionsTemplate < OptionsTemplate
+module IPFIX
+  class IETypeOptionsTemplate < OptionsTemplate
   
-  def initialize(model, tid)
-    super(model, tid)
-    add_scope "informationElementId"
-    add_scope "privateEnterpiseNumber"
-    << "informationElementDataType"
-    << "informationElementName"
+    def initialize(model, tid)
+      super(model, tid)
+      add_scope "informationElementId"
+      add_scope "privateEnterpiseNumber"
+      this << "informationElementDataType"
+      this << "informationElementName"
+    end
+
+    def self.is_type_options_record(h)
+      h[:informationElementId] && h[:privateEnterpriseNumber] && h[:informationElementDataType]
+    end
+
   end
 
-  def self.is_type_options_record(h)
-    h[:informationElementId] && h[:privateEnterpriseNumber] && h[:informationElementDataType]
-  end
-
-end
-
-class InfoModel
-  def add_type_options_record(h)
-    if (IETypeOptionsTemplate.is_type_options_record(h))
-      add(InfoElement.new(h[:informationElementName],
-                          h[:privateEnterpriseNumber], 
-                          h[:informationElementId],
-                   @types[h[:informationElementDataType]]))
+  class InfoModel
+    def add_type_options_record(h)
+      if (IETypeOptionsTemplate.is_type_options_record(h))
+        add(InfoElement.new(h[:informationElementName],
+                            h[:privateEnterpriseNumber], 
+                            h[:informationElementId],
+                     @types[h[:informationElementDataType]]))
+      end
     end
   end
-end
 
-class InfoElement
-  def type_options_record
-    h = Hash.new
-    h[:informationElementId] = number
-    h[:privateEnterpriseNumber] = pen
-    h[:informationElementDataType] = type.number
-    h[:informationElementName] = name
+  class InfoElement
+    def type_options_record
+      h = Hash.new
+      h[:informationElementId] = number
+      h[:privateEnterpriseNumber] = pen
+      h[:informationElementDataType] = type.number
+      h[:informationElementName] = name
+    end
   end
 end
